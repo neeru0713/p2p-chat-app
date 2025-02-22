442f2cc70e1965f1739a30fab0e33740f327f80b
neeru0713 <neerurani1307@gmail.com>
2025-02-23 00:04:35 +0530

    created component chatpage , chats, chatarea and message

---
+++ src/components/ChatArea.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ChatArea = () => {
+  return (
+    
+      <div className="flex w-[70%]">
+        
+              
+       
+      </div>
+
+  );
+}
+
+export default ChatArea
